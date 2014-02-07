9248fda0a62c78bc6d48014b38c1c8fa3b24c4ab
Peter Bacinsky <peter@bacinsky.sk>
2014-02-07 09:17:22 +0100

    Updated example module config comments

--- example/config/module.config.php
+++ example/config/module.config.php
@@ -1,3 +1,14 @@
 <?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDbDump for the canonical source repository
+ * @copyright   Copyright (c) 2014 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     The BSD 3-Clause License
+ */
 
+/**
+ * For more examples check out following file
+ */
 return require __DIR__ . '/../../config/webino_dbdump.global.php.dist';
