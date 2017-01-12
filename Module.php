e73dd5bed7370b816905b3be848470793c1b2ff7
Peter Bacinsky <peter@bacinsky.sk>
2017-01-12 09:28:59 +0100

    Slightly beautified

--- Module.php
+++ Module.php
@@ -3,9 +3,9 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDbDump/ for the canonical source repository
- * @copyright   Copyright (c) 2014-2016 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2014-2017 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
- * @license     The BSD 3-Clause License
+ * @license     BSD-3-Clause
  */
 
 include __DIR__ . '/src/WebinoDbDump/Module.php';
